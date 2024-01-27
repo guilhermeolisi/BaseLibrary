7c675238cfe33b216c0d8a21c44afbe3536c77b9
Guilherme Oliveira <guilhermeolisi@outlook.com>
2024-01-26 21:51:33 -0300

    Bakcup mudança para Avalonia 11

--- Baselibrary/SystemUtility.cs
+++ Baselibrary/SystemUtility.cs
@@ -36,15 +36,11 @@ public static class SystemUtility
         }
         return (platform, os, is64x);
     }
-    /// <summary>
-    /// https://stackoverflow.com/questions/2819934/detect-windows-version-in-net
-    /// </summary>
-    /// <returns>0: 95; 1: 98; 2: 98SE; 3: Me; 4: NT 3.51; 5: NT 4.0; 6: 2000; 7: XP; 8: Vsta; 9: 7; 10: 8; 11: 8.1; 12: 10; 13: 11</returns>
-    public static sbyte WindowsVersion()
+    public static WindowsVersion WindowsVersionCheck()
     {
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            return -1;
+            return WindowsVersion.Unknown;
         }
         //Get Operating system information.
         OperatingSystem os = Environment.OSVersion;
@@ -52,7 +48,7 @@ public static class SystemUtility
         Version vs = os.Version;
 
         //Variable to hold our return value
-        sbyte operatingSystem = -1;
+        WindowsVersion operatingSystem = WindowsVersion.Unknown;
 
         if (os.Platform == PlatformID.Win32Windows)
         {
@@ -60,16 +56,16 @@ public static class SystemUtility
             switch (vs.Minor)
             {
                 case 0:
-                    operatingSystem = 0; //"95";
+                    operatingSystem = WindowsVersion.Windows95; //"95";
                     break;
                 case 10:
                     if (vs.Revision.ToString() == "2222A")
-                        operatingSystem = 2; //"98SE";
+                        operatingSystem = WindowsVersion.Windows98SE; //"98SE";
                     else
-                        operatingSystem = 1; //"98";
+                        operatingSystem = WindowsVersion.Windows98; //"98";
                     break;
                 case 90:
-                    operatingSystem = 3; //"Me";
+                    operatingSystem = WindowsVersion.WindowsMe; //"Me";
                     break;
                 default:
                     break;
@@ -80,40 +76,47 @@ public static class SystemUtility
             switch (vs.Major)
             {
                 case 3:
-                    operatingSystem = 4; // "NT 3.51";
+                    operatingSystem = WindowsVersion.WindowsNT351; // "NT 3.51";
                     break;
                 case 4:
-                    operatingSystem = 5; //"NT 4.0";
+                    operatingSystem = WindowsVersion.WindowsNT40; //"NT 4.0";
                     break;
                 case 5:
                     if (vs.Minor == 0)
-                        operatingSystem = 6; // "2000";
+                        operatingSystem = WindowsVersion.Windows2000; // "2000";
                     else
-                        operatingSystem = 7; // "XP";
+                        operatingSystem = WindowsVersion.WindowsXP; // "XP";
                     break;
                 case 6:
                     if (vs.Minor == 0)
-                        operatingSystem = 8;// "Vista";
+                        operatingSystem = WindowsVersion.WindowsVista;// "Vista";
                     else if (vs.Minor == 1)
-                        operatingSystem = 9;// "7";
+                        operatingSystem = WindowsVersion.Windows7;// "7";
                     else if (vs.Minor == 2)
-                        operatingSystem = 10;// "8";
+                        operatingSystem = WindowsVersion.Windows8;// "8";
                     else
-                        operatingSystem = 11;// "8.1";
+                        operatingSystem = WindowsVersion.Windows81;// "8.1";
                     break;
                 case 10:
-                    operatingSystem = 12;// "10";
+                    if (vs.Build < 22000)
+                    {
+                        operatingSystem = WindowsVersion.Windows10;// "10";
+                    }
+                    else
+                    {
+                        operatingSystem = WindowsVersion.Windows11;// "11";
+                    }
                     break;
-                case 11:
-                    operatingSystem = 13;// "11";
-                    break;
+                //case 11:
+                //    operatingSystem = WindowsVersion.Windows11;// "11";
+                //    break;
                 default:
                     break;
             }
         }
         return operatingSystem;
     }
-    public static string SystemVersionToString(sbyte OS, sbyte version)
+    public static string SystemVersionToString(sbyte OS, WindowsVersion version)
     {
         string result = (OS == 0 ? "Windows" : OS == 1 ? "Linux" : OS == 2 ? "macOS" : null);
         if (string.IsNullOrWhiteSpace(result))
@@ -123,25 +126,151 @@ public static class SystemUtility
             case 0:
                 result += " " + version switch
                 {
-                    0 => "95",
-                    2 => "98SE",
-                    1 => "98",
-                    3 => "Me",
-                    4 => "NT 3.51",
-                    5 => "NT 4.0",
-                    6 => "2000",
-                    7 => "XP",
-                    8 => "Vista",
-                    9 => "7",
-                    10 => "8",
-                    11 => "8.1",
-                    12 => "10",
-                    13 => "11",
+                    WindowsVersion.Windows95 => "95",
+                    WindowsVersion.Windows98SE => "98SE",
+                    WindowsVersion.Windows98 => "98",
+                    WindowsVersion.WindowsMe => "Me",
+                    WindowsVersion.WindowsNT351 => "NT 3.51",
+                    WindowsVersion.WindowsNT40 => "NT 4.0",
+                    WindowsVersion.Windows2000 => "2000",
+                    WindowsVersion.WindowsXP => "XP",
+                    WindowsVersion.WindowsVista => "Vista",
+                    WindowsVersion.Windows7 => "7",
+                    WindowsVersion.Windows8 => "8",
+                    WindowsVersion.Windows81 => "8.1",
+                    WindowsVersion.Windows10 => "10",
+                    WindowsVersion.Windows11 => "11",
+                    WindowsVersion.Unknown => "Unknown",
                     _ => ""
                 };
                 break;
         }
         return result.Trim();
+    }
+    /// <summary>
+    /// https://stackoverflow.com/questions/2819934/detect-windows-version-in-net
+    /// </summary>
+    /// <returns>0: 95; 1: 98; 2: 98SE; 3: Me; 4: NT 3.51; 5: NT 4.0; 6: 2000; 7: XP; 8: Vsta; 9: 7; 10: 8; 11: 8.1; 12: 10; 13: 11</returns>
+    //public static sbyte WindowsVersionCheck()
+    //{
+    //    if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    //    {
+    //        return -1;
+    //    }
+    //    //Get Operating system information.
+    //    OperatingSystem os = Environment.OSVersion;
+    //    //Get version information about the os.
+    //    Version vs = os.Version;
+
+    //    //Variable to hold our return value
+    //    sbyte operatingSystem = -1;
+
+    //    if (os.Platform == PlatformID.Win32Windows)
+    //    {
+    //        //This is a pre-NT version of Windows
+    //        switch (vs.Minor)
+    //        {
+    //            case 0:
+    //                operatingSystem = 0; //"95";
+    //                break;
+    //            case 10:
+    //                if (vs.Revision.ToString() == "2222A")
+    //                    operatingSystem = 2; //"98SE";
+    //                else
+    //                    operatingSystem = 1; //"98";
+    //                break;
+    //            case 90:
+    //                operatingSystem = 3; //"Me";
+    //                break;
+    //            default:
+    //                break;
+    //        }
+    //    }
+    //    else if (os.Platform == PlatformID.Win32NT)
+    //    {
+    //        switch (vs.Major)
+    //        {
+    //            case 3:
+    //                operatingSystem = 4; // "NT 3.51";
+    //                break;
+    //            case 4:
+    //                operatingSystem = 5; //"NT 4.0";
+    //                break;
+    //            case 5:
+    //                if (vs.Minor == 0)
+    //                    operatingSystem = 6; // "2000";
+    //                else
+    //                    operatingSystem = 7; // "XP";
+    //                break;
+    //            case 6:
+    //                if (vs.Minor == 0)
+    //                    operatingSystem = 8;// "Vista";
+    //                else if (vs.Minor == 1)
+    //                    operatingSystem = 9;// "7";
+    //                else if (vs.Minor == 2)
+    //                    operatingSystem = 10;// "8";
+    //                else
+    //                    operatingSystem = 11;// "8.1";
+    //                break;
+    //            case 10:
+    //                operatingSystem = 12;// "10";
+    //                break;
+    //            case 11:
+    //                operatingSystem = 13;// "11";
+    //                break;
+    //            default:
+    //                break;
+    //        }
+    //    }
+    //    return operatingSystem;
+    //}
+    //public static string SystemVersionToString(sbyte OS, sbyte version)
+    //{
+    //    string result = (OS == 0 ? "Windows" : OS == 1 ? "Linux" : OS == 2 ? "macOS" : null);
+    //    if (string.IsNullOrWhiteSpace(result))
+    //        return result;
+    //    switch (OS)
+    //    {
+    //        case 0:
+    //            result += " " + version switch
+    //            {
+    //                0 => "95",
+    //                2 => "98SE",
+    //                1 => "98",
+    //                3 => "Me",
+    //                4 => "NT 3.51",
+    //                5 => "NT 4.0",
+    //                6 => "2000",
+    //                7 => "XP",
+    //                8 => "Vista",
+    //                9 => "7",
+    //                10 => "8",
+    //                11 => "8.1",
+    //                12 => "10",
+    //                13 => "11",
+    //                _ => ""
+    //            };
+    //            break;
+    //    }
+    //    return result.Trim();
+    //}
+    public enum WindowsVersion : sbyte
+    {
+        Unknown = -1,
+        Windows95,
+        Windows98,
+        Windows98SE,
+        WindowsMe,
+        WindowsNT351,
+        WindowsNT40,
+        Windows2000,
+        WindowsXP,
+        WindowsVista,
+        Windows7,
+        Windows8,
+        Windows81,
+        Windows10,
+        Windows11
     }
 }
 
